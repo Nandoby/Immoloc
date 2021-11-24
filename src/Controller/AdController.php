998314dd1dbab1adee9cbcba23cb5b694db9f2aa
Nandoby <nandobiaccalli@gmail.com>
2021-11-24 21:12:19 +0100

    edition

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -37,15 +37,13 @@ class AdController extends AbstractController
 
         $image1 = new Image();
         $image1->setUrl("http://placehold.it/400x200")
-            ->setCaption("Titre 1")
-        ;
+            ->setCaption("Titre 1");
 
         $ad->addImage($image1);
 
         $image2 = new Image();
         $image2->setUrl("http://placehold.it/400x200")
-            ->setCaption("Titre 2")
-        ;
+            ->setCaption("Titre 2");
         $ad->addImage($image2);
 
         $form = $this->createForm(AnnonceType::class, $ad);
@@ -53,7 +51,7 @@ class AdController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
 
-            foreach($ad->getImages() as $image) {
+            foreach ($ad->getImages() as $image) {
                 $image->setAd($ad);
                 $manager->persist($image);
             }
@@ -81,6 +79,36 @@ class AdController extends AbstractController
     public function show($slug, Ad $ad): Response
     {
         return $this->render('ad/show.html.twig', [
+            'ad' => $ad
+        ]);
+    }
+
+    /**
+     * Permet de modifier une annonce
+     * @Route("/ads/{slug}/edit", name="ads_edit")
+     */
+    public function edit(Request $request, EntityManagerInterface $manager, Ad $ad): Response
+    {
+        $form = $this->createForm(AnnonceType::class, $ad);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            foreach($ad->getImages() as $image) {
+                $image->setAd($ad);
+                $manager->persist($image);
+            }
+
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "L'annonce <strong>{$ad->getTitle()}</strong> a bien été modifiée !"
+            );
+        }
+
+        return $this->render("ad/edit.html.twig", [
+            "form" => $form->createView(),
             'ad' => $ad
         ]);
     }
