ed08bb014d996c02bc1026d578abb42ae45667a3
Nandoby <nandobiaccalli@gmail.com>
2021-11-24 10:51:15 +0100

    insertion + message flush

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -5,8 +5,10 @@ namespace App\Controller;
 use App\Entity\Ad;
 use App\Form\AnnonceType;
 use App\Repository\AdRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -28,10 +30,24 @@ class AdController extends AbstractController
      * Permet de créer une annonce
      * @Route("/ads/new", name="ads_create")
      */
-    public function create(): Response
+    public function create(Request $request, EntityManagerInterface $manager): Response
     {
         $ad = new Ad();
         $form = $this->createForm(AnnonceType::class, $ad);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "L'annonce <strong>{$ad->getTitle()}</strong> a bien été enregistrée !"
+            );
+
+            return $this->redirectToRoute('ads_show', ['slug' => $ad->getSlug()]);
+        }
+
         return $this->render("ad/new.html.twig", [
             'form' => $form->createView()
         ]);
