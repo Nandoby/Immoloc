ed5403fda2ec1b61bf63c11014ac05003bffda3c
Nandoby <nandobiaccalli@gmail.com>
2021-11-24 09:43:20 +0100

    form etape 1

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -23,6 +23,27 @@ class AdController extends AbstractController
     }
 
     /**
+     * Permet de créer une annonce
+     * @Route("/ads/new", name="ads_create")
+     */
+    public function create(): Response
+    {
+        $ad = new Ad();
+        $form = $this->createFormBuilder($ad)
+            ->add('title')
+            ->add('introduction')
+            ->add('content')
+            ->add('rooms')
+            ->add('price')
+            ->getForm()
+        ;
+
+        return $this->render('ad/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
      * Permet d'afficher une seule annonce
      * @Route("/ads/{slug}", name="ads_show")
      */
